fde520e4b123cbb386a9f1cb4a1377bfcfb397d8
Nguyen Trung Truc(尚紀) <132861531+Naoki031@users.noreply.github.com>
2024-10-24 16:51:35 +0700

    feat: role management feature

--- src/modules/roles/roles.module.ts
+++ src/modules/roles/roles.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { RolesController } from './roles.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { rolesProviders } from './roles.provider';
+import { Role } from './entities/role.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Role])],
   controllers: [RolesController],
-  providers: [RolesService],
+  providers: [RolesService, ...rolesProviders],
 })
 export class RolesModule {}
--- src/modules/roles/roles.service.ts
+++ src/modules/roles/roles.service.ts
@@ -1,26 +1,74 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Role } from './entities/role.entity';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
 
 @Injectable()
 export class RolesService {
-  create(createRoleDto: CreateRoleDto) {
-    return 'This action adds a new role';
+  constructor(
+    @InjectRepository(Role)
+    private readonly roleRepository: Repository<Role>,
+  ) {}
+
+  /**
+   * Creates a new role using the provided data transfer object.
+   *
+   * @param createRoleDto - The data transfer object containing the details of the role to be created.
+   * @returns A promise that resolves to the created role.
+   */
+  create(createRoleDto: CreateRoleDto): Promise<Role> {
+    const role = this.roleRepository.save(createRoleDto);
+
+    return role;
   }
 
-  findAll() {
-    return `This action returns all roles`;
+  /**
+   * Retrieves all roles from the repository.
+   *
+   * @returns {Promise<Role[]>} A promise that resolves to an array of roles.
+   */
+  findAll(): Promise<Role[]> {
+    const roles = this.roleRepository.find();
+
+    return roles;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} role`;
+  /**
+   * Finds a role by its ID.
+   *
+   * @param {number} id - The ID of the role to find.
+   * @returns {Promise<Role>} A promise that resolves to the found role.
+   */
+  findOne(id: number): Promise<Role> {
+    const role = this.roleRepository.findOne({ where: { id } });
+
+    return role;
   }
 
-  update(id: number, updateRoleDto: UpdateRoleDto) {
-    return `This action updates a #${id} role`;
+  /**
+   * Updates an existing role with the provided data.
+   *
+   * @param {number} id - The ID of the role to update.
+   * @param {UpdateRoleDto} updateRoleDto - The data to update the role with.
+   * @returns {Promise<Role>} A promise that resolves to the updated role.
+   */
+  update(id: number, updateRoleDto: UpdateRoleDto): Promise<Role> {
+    const role = this.roleRepository.save({ id, ...updateRoleDto });
+
+    return role;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} role`;
+  /**
+   * Removes a role by its ID.
+   *
+   * @param {number} id - The ID of the role to remove.
+   * @returns {Promise<boolean>} A promise that resolves to `true` if the role was successfully removed, otherwise `false`.
+   */
+  remove(id: number): Promise<boolean> {
+    return this.roleRepository
+      .delete(id)
+      .then((result) => result.affected === 1);
   }
 }
