459795c71985a48abce58fb874362bd4477684c8
Nguyen Trung Truc <trucnguyen.remvn031@gmail.com>
2026-03-29 12:34:36 +0700

    feat: team management

---
+++ src/modules/groups/groups.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { GroupsController } from './groups.controller'
+import { GroupsService } from './groups.service'
+import { Group } from './entities/group.entity'
+import { UserGroup } from './entities/user_group.entity'
+import { UserGroupPermissionsModule } from '@/modules/user_group_permissions/user_group_permissions.module'
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Group, UserGroup]), UserGroupPermissionsModule],
+  controllers: [GroupsController],
+  providers: [GroupsService],
+  exports: [GroupsService],
+})
+export class GroupsModule {}
